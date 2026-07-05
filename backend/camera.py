d67fc690225c245dbd0457f59e967d0344d8fa1d
xiyuexu-1013 <xiyue.xu@sbs.org>
2026-07-05 12:08:21 +0800

    Create camera.py

---
+++ backend/camera.py
@@ -0,0 +1,16 @@
+import cv2
+
+class Camera:
+    def __init__(self, camera_index=0):
+        self.cap = cv2.VideoCapture(camera_index)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+    def get_frame(self):
+        ret, frame = self.cap.read()
+        if not ret:
+            return None
+        return frame
+
+    def release(self):
+        self.cap.release()
